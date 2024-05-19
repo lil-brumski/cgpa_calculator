8c3d3d8240d921b75d9bc557afc69ec8e3555bb8
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-05-19 18:16:23 +0100

    Update BRUMSKI_GRADE_CALCULATOR.hpp

--- BRUMSKI_GRADE_CALCULATOR.hpp
+++ BRUMSKI_GRADE_CALCULATOR.hpp
@@ -7,27 +7,28 @@
 #define BRUMSKI_GRADE_CALC
 
 #include <string>
+#include <sstream>
 #include <iostream>
 #include <limits>
 
 namespace BRUMSKI {
 
      template <class T>
-          T Input(const std::string& prompt){
-              T value;
-              while(true){
-                  std::cout<<prompt;
-                  if(std::cin>>value){
-                      break;
-                      }
-                  else{
-                      std::cin.clear();
-                      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                      std::cerr<<"Wrong data type, try again!\n";
-                      }
-                  }
-               return value;
+       T Input(const std::string& prompt) {
+          T value;
+          std::string input;
+         while (true) {
+            std::cout << prompt;
+            std::getline(std::cin, input);
+            std::istringstream stream(input);
+            if (stream >> value && stream.eof()) {
+                 break;
+            } else {
+                 std::cerr << "Wrong data type, try again!\n";
               }
+        }
+        return value;
+     }
 
     namespace GRADE{       
 
