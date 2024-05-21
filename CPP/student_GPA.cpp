4cee138b5ed207cd245c660fe80be46884c97fe3
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-05-21 11:51:22 +0100

    Add files via upload

--- CPP/student_GPA.cpp
+++ CPP/student_GPA.cpp
@@ -6,7 +6,8 @@
 
 
 int main(){
-
+    
+    std::cout<<"FIRST SEMESTER:\n";
     //Variables for storing the scores.
     double RSU_GET103 = BRUMSKI::Input<double>("Enter your score for Elementary Mathematics III: ");
     while(RSU_GET103 < 0 || RSU_GET103 > 100){
@@ -150,7 +151,8 @@ int main(){
 
    double total_credit_unit = 15;
    double GPA = 0.00;
-   GPA = (rsuGET103GP + cpe111GP + chm101GP + mth101GP + phy101GP + chm107GP + get101GP + phy107GP + gst111GP) / total_credit_unit;
+   double cumulative = (rsuGET103GP + cpe111GP + chm101GP + mth101GP + phy101GP + chm107GP + get101GP + phy107GP + gst111GP);
+   GPA = cumulative / total_credit_unit;
 
    std::cout<<std::endl;
 
@@ -173,10 +175,168 @@ int main(){
 
    std::cout<<"GST111: "<<gst111<<std::endl;
 
-   std::cout<<"This semester GPA: "<<std::fixed<<std::setprecision(2)<<GPA<<std::endl;
+   std::cout<<"First semester GPA: "<<std::fixed<<std::setprecision(2)<<GPA<<std::endl;
 
    std::cout<<"No. of carry over courses: "<<carry_over<<std::endl;
    std::cout<<"No. of passed courses: "<<(9-carry_over);
    std::cout<<std::endl;
+
+
+
+
+
+//SECOND Semester 
+    std::cout<<"\nSECOND SEMESTER:\n";
+    //Variables for storing the scores.
+    double RSU_GET104 = BRUMSKI::Input<double>("Enter your score for General Physics IV: ");
+    while(RSU_GET104 < 0 || RSU_GET104 > 100){
+        std::cout<<"Score can't be lower than 0 or higher than 100. Try again."<<std::endl;
+        RSU_GET104 = BRUMSKI::Input<double>("Enter your score for General Physics IV: ");
+    }
+
+    double RSU_GET112 = BRUMSKI::Input<double>("Enter your score for Probability I: ");
+    while(RSU_GET112 < 0 || RSU_GET112 > 100){
+        std::cout<<"Score can't be lower than 0 or higher than 100. Try again."<<std::endl;
+        RSU_GET112 = BRUMSKI::Input<double>("Enter your score for Probability I: ");
+    }
+
+    double CHM102 = BRUMSKI::Input<double>("Enter your score for General Chemistry II: ");
+    while(CHM102 < 0 || CHM102 > 100){
+        std::cout<<"Score can't be lower than 0 or higher than 100. Try again."<<std::endl;
+        CHM102 = BRUMSKI::Input<double>("Enter your score for General Chemistry II: ");
+    }
+
+    double MTH102 = BRUMSKI::Input<double>("Enter your score for Elementary Mathematics II: ");
+    while(MTH102 < 0 || MTH102 > 100){
+        std::cout<<"Score can't be lower than 0 or higher than 100. Try again."<<std::endl;
+        MTH102 = BRUMSKI::Input<double>("Enter your score for Elementary Mathematics II: ");
+    }
+
+    double PHY102 = BRUMSKI::Input<double>("Enter your score for General Physics II: ");
+    while(PHY102 < 0 || PHY102 > 100){
+        std::cout<<"Score can't be lower than 0 or higher than 100. Try again."<<std::endl;
+        PHY102 = BRUMSKI::Input<double>("Enter your score for General Physics II: ");
+    }
+
+    double CHM108 = BRUMSKI::Input<double>("Enter your score for General Practical Chemistry II: ");
+    while(CHM108 < 0 || CHM108 > 100){
+        std::cout<<"Score can't be lower than 0 or higher than 100. Try again."<<std::endl;
+        CHM108 = BRUMSKI::Input<double>("Enter your score for General Practical Chemistry II: ");
+    }
+
+    double GET102 = BRUMSKI::Input<double>("Enter your score for Engineering Graphics and Solid Modelling: ");
+    while(GET102 < 0 || GET102 > 100){
+        std::cout<<"Score can't be lower than 0 or higher than 100. Try again."<<std::endl;
+        GET102 = BRUMSKI::Input<double>("Enter your score for Engineering Graphics and Solid Modelling: ");
+    }
+
+    double PHY108 = BRUMSKI::Input<double>("Enter your score for General Practical Physics II: ");
+    while(PHY108 < 0 || PHY108 > 100){
+        std::cout<<"Score can't be lower than 0 or higher than 100. Try again."<<std::endl;
+        PHY108 = BRUMSKI::Input<double>("Enter your score for General Practical Physics II: ");
+    }
+
+    double GST112 = BRUMSKI::Input<double>("Enter your score for Nigerian People and Culture: ");
+    while(GST112 < 0 || GST112 > 100){
+        std::cout<<"Score can't be lower than 0 or higher than 100. Try again."<<std::endl;
+        GST112 = BRUMSKI::Input<double>("Enter your score for Nigerian People and Culture: ");
+    }
+
+    //Rounds the scores to the nearest integer.
+    RSU_GET104 = std::round(RSU_GET104);
+    RSU_GET112 = std::round(RSU_GET112);
+    CHM102 = std::round(CHM102);
+    MTH102 = std::round(MTH102);
+    PHY102 = std::round(PHY102);
+    CHM108 = std::round(CHM108);
+    GET102 = std::round(GET102);
+    PHY108 = std::round(PHY108);
+    GST112 = std::round(GST112);
+
+    //The grades and grade points.
+    char rsu_get104 = BRUMSKI::GRADE::CALCULATOR(RSU_GET104);
+    double rsuGET104GP = BRUMSKI::GRADE::POINT(rsu_get104, 2);
+
+    char rsu_get112 = BRUMSKI::GRADE::CALCULATOR(RSU_GET112);
+    double rsuGET112GP = BRUMSKI::GRADE::POINT(rsu_get112, 3);
+
+    char chm102 = BRUMSKI::GRADE::CALCULATOR(CHM102);
+    double chm102GP = BRUMSKI::GRADE::POINT(chm102, 2);
+
+    char mth102 = BRUMSKI::GRADE::CALCULATOR(MTH102);
+    double mth102GP = BRUMSKI::GRADE::POINT(mth102, 2);
+
+    char phy102 = BRUMSKI::GRADE::CALCULATOR(PHY102);
+    double phy102GP = BRUMSKI::GRADE::POINT(phy102, 2);
+
+    char chm108 = BRUMSKI::GRADE::CALCULATOR(CHM108);
+    double chm108GP = BRUMSKI::GRADE::POINT(chm108, 1);
+
+    char get102 = BRUMSKI::GRADE::CALCULATOR(GET102);
+    double get102GP = BRUMSKI::GRADE::POINT(get102, 3);
+
+    char phy108 = BRUMSKI::GRADE::CALCULATOR(PHY108);
+    double phy108GP = BRUMSKI::GRADE::POINT(phy108, 1);
+
+    char gst112 = BRUMSKI::GRADE::CALCULATOR(GST112);
+    double gst112GP = BRUMSKI::GRADE::POINT(gst112, 2);
+
+    int carry_over_sec = 0;
+
+    if(rsu_get104 == 'F'){
+        carry_over_sec++;
+    }
+    if(rsu_get112 == 'F'){
+        carry_over_sec++;
+    } 
+    if(chm102 == 'F'){
+        carry_over_sec++;
+    }
+    if(mth102 == 'F'){
+        carry_over_sec++;
+    }
+    if(phy102 == 'F'){
+        carry_over_sec++;
+    }
+    if(chm108 == 'F'){
+        carry_over_sec++;
+    }
+    if(get102 == 'F'){
+        carry_over_sec++;
+    }
+    if(phy108 == 'F'){
+        carry_over_sec++;
+    }
+    if(gst112 == 'F'){
+        carry_over_sec++;
+    }
+
+    double total_credit_unit_sec = 18;
+    double GPA_sec = 0.00;
+    double cumulative_sec = (rsuGET104GP + rsuGET112GP + chm102GP + mth102GP + phy102GP + chm108GP + get102GP + phy108GP + gst112GP);
+    GPA =  cumulative_sec / total_credit_unit_sec;
+
+    std::cout<<std::endl;
+
+    //Message
+    std::cout<<"RSU-GET104: "<<rsu_get104<<std::endl;
+    std::cout<<"RSU-GET112: "<<rsu_get112<<std::endl;
+    std::cout<<"CHM102: "<<chm102<<std::endl;
+    std::cout<<"MTH102: "<<mth102<<std::endl;
+    std::cout<<"PHY102: "<<phy102<<std::endl;
+    std::cout<<"CHM108: "<<chm108<<std::endl;
+    std::cout<<"GET102: "<<get102<<std::endl;
+    std::cout<<"PHY108: "<<phy108<<std::endl;
+    std::cout<<"GST112: "<<gst112<<std::endl;
+
+    std::cout<<"Second semester GPA: "<<std::fixed<<std::setprecision(2)<<GPA<<std::endl;
+    std::cout<<"No. of carry over courses: "<<carry_over_sec<<std::endl;
+    std::cout<<"No. of passed courses: "<<(9 - carry_over_sec);
+    std::cout<<std::endl;
+    
+    double CGPA = (cumulative + cumulative_sec) / (total_credit_unit + total_credit_unit_sec);
+
+   std::cout<<"CGPA: "<<std::fixed<<std::setprecision(2)<<CGPA<<std::endl;
+    
 
 }
