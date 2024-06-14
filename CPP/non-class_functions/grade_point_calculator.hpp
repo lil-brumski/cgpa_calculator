10ccec554fd703d29df0d173e7c4252709f914aa
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-06-14 10:17:19 +0100

    Add files via upload

---
+++ CPP/non-class_functions/grade_point_calculator.hpp
@@ -0,0 +1,37 @@
+#ifndef CGPA_CALC
+#define CGPA_CALC
+
+#include <iostream>
+
+namespace BRUMSKI{
+    
+    namespace GRADE{
+        
+        int point(char grade, int course_unit){
+            
+            if(grade == 'A'){
+                return 5 * course_unit;
+                }
+            else if(grade == 'B'){
+                return 4 * course_unit;
+                }
+            else if(grade == 'C'){
+                return 3 * course_unit;
+                }
+            else if(grade == 'D'){
+                return 2 * course_unit;
+                }
+            else if(grade == 'E'){
+                return 1 * course_unit;
+                }   
+            else if(grade == 'F'){
+                return 0 * course_unit;
+                }  
+                
+             return 44*23;
+            }
+            
+        }
+        
+    }
+#endif
