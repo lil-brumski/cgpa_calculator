010cb0088004d0b78532bf5da07e342bc287c2b8
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-06-14 11:39:32 +0100

    Add files via upload

---
+++ CPP/headers.hpp
@@ -0,0 +1,16 @@
+#ifndef HEADER_MAIN
+#define HEADER_MAIN
+
+
+#include "class_implementations/CGPA/student_cgpa_part1.hpp"
+
+#include "class_implementations/CGPA/student_cgpa_part2.hpp"
+
+#include "class_implementations/CGPA/student_cgpa_part3.hpp"
+
+#include "class_implementations/GPA/student_gpa_1.hpp"
+
+#include "class_implementations/GPA/student_gpa_2.hpp"
+
+
+#endif
